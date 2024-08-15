36bbc3acfbc2cea83f3971c85f1da1f4ded0eccc
antonelavargas <avargasrobaina@gmail.com>
2024-08-14 21:21:34 -0300

    agregue js, estilos al boton

---
+++ carrusel.js
@@ -0,0 +1,16 @@
+let diapositivaActual = 0;
+const diapositivas = document.querySelectorAll('.diapositiva-carrusel');
+
+function mostrarDiapositiva(indice) {
+    diapositivas[diapositivaActual].classList.remove('activa');
+    diapositivaActual = (indice + diapositivas.length) % diapositivas.length;
+    diapositivas[diapositivaActual].classList.add('activa');
+}
+
+function diapositivaSiguiente() {
+    mostrarDiapositiva(diapositivaActual + 1);
+}
+
+function diapositivaAnterior() {
+    mostrarDiapositiva(diapositivaActual - 1);
+}
